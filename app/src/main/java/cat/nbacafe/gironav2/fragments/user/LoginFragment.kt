53215a47f1a101b2c65e7393745034246f8ff4d2
mcfl-uri <lamelasoriol@gmail.com>
2022-01-19 17:38:45 +0100

    primers fragments creats

---
+++ app/src/main/java/cat/nbacafe/gironav2/fragments/user/LoginFragment.kt
@@ -0,0 +1,24 @@
+package cat.nbacafe.gironav2.fragments.user
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import cat.nbacafe.gironav2.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [LoginFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+}
