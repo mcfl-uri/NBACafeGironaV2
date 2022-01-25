65158080ba68815f0afb93497745ca64dc2b03bd
mcfl-uri <lamelasoriol@gmail.com>
2022-01-25 17:44:29 +0100

    firebase auth maomeno

--- app/src/main/java/cat/nbacafe/gironav2/fragments/user/LoginFragment.kt
+++ app/src/main/java/cat/nbacafe/gironav2/fragments/user/LoginFragment.kt
@@ -4,21 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import cat.nbacafe.gironav2.R
+import cat.nbacafe.gironav2.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LoginFragment : Fragment() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val binding = DataBindingUtil.inflate<FragmentLoginBinding>(
+            inflater,
+            R.layout.fragment_login, container, false
+        )
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.registraBtn.setOnClickListener { View ->
+            view?.findNavController()?.navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+
+        return binding.root
     }
 }
--- app/src/main/java/cat/nbacafe/gironav2/fragments/user/RegisterFragment.kt
+++ app/src/main/java/cat/nbacafe/gironav2/fragments/user/RegisterFragment.kt
@@ -4,15 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import cat.nbacafe.gironav2.R
+import cat.nbacafe.gironav2.databinding.FragmentLoginBinding
+import cat.nbacafe.gironav2.databinding.FragmentRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterFragment : Fragment() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        val binding = DataBindingUtil.inflate<FragmentRegisterBinding>(
+            inflater,
+            R.layout.fragment_register, container, false
+        )
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.createUserBtn.setOnClickListener { View ->
+            auth.createUserWithEmailAndPassword(binding.emailEditText.text.toString(), binding.passEditText.text.toString())
+        }
+
+        return binding.root
     }
 }
