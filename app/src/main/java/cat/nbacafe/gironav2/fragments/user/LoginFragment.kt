fc039946626d6413d4c05ca66f8567ff1cca216f
mcfl-uri <lamelasoriol@gmail.com>
2022-01-26 18:16:55 +0100

    google auth

--- app/src/main/java/cat/nbacafe/gironav2/fragments/user/LoginFragment.kt
+++ app/src/main/java/cat/nbacafe/gironav2/fragments/user/LoginFragment.kt
@@ -1,5 +1,6 @@
 package cat.nbacafe.gironav2.fragments.user
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,11 +11,29 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import cat.nbacafe.gironav2.R
 import cat.nbacafe.gironav2.databinding.FragmentLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginFragment : Fragment() {
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+    companion object {
+        const val GOOGLE_SIGN_IN = 303
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        auth = Firebase.auth
+        googleSignInClient = GoogleSignIn.getClient(requireContext(), getGSO())
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -51,6 +70,45 @@ class LoginFragment : Fragment() {
             view?.findNavController()?.navigate(R.id.action_loginFragment_to_registerFragment)
         }
 
+        binding.googleAuthBtn.setOnClickListener { View ->
+
+            val signInIntent = googleSignInClient.signInIntent
+            startActivityForResult(signInIntent, GOOGLE_SIGN_IN)
+
+        }
+
         return binding.root
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == GOOGLE_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)!!
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                //handle error
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    view?.findNavController()?.navigate(R.id.action_loginFragment_to_homeFragment)
+                } else {
+                    Toast.makeText(context, "S'ha produït un error", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun getGSO(): GoogleSignInOptions {
+        return  GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
     }
 }
