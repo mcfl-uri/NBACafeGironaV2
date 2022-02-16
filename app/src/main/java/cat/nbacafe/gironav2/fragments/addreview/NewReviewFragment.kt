5a83dd1f5557238eff349a81181a0c4cba1bf96d
mcfl-uri <lamelasoriol@gmail.com>
2022-02-16 14:50:26 +0100

    rebre item a new review

--- app/src/main/java/cat/nbacafe/gironav2/fragments/addreview/NewReviewFragment.kt
+++ app/src/main/java/cat/nbacafe/gironav2/fragments/addreview/NewReviewFragment.kt
@@ -1,16 +1,18 @@
 package cat.nbacafe.gironav2.fragments.addreview
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import cat.nbacafe.gironav2.R
 import cat.nbacafe.gironav2.common.SharedViewModel
 import cat.nbacafe.gironav2.databinding.FragmentNewReviewBinding
+import com.google.firebase.storage.FirebaseStorage
+import java.io.File
 
 class NewReviewFragment : Fragment() {
 
@@ -25,7 +27,19 @@ class NewReviewFragment : Fragment() {
             R.layout.fragment_new_review, container, false
         )
 
-        Toast.makeText(context, sharedViewModel.getItem().nom, Toast.LENGTH_LONG).show()
+        val item = sharedViewModel.getItem()
+
+        val storage = FirebaseStorage.getInstance().reference.child(item.nomImatge + ".jpg")
+        val imatge = File.createTempFile("img", "jpg")
+        storage.getFile(imatge).addOnSuccessListener {
+            val bitmap = BitmapFactory.decodeFile(imatge.absolutePath)
+            binding.topImage.setImageBitmap(bitmap)
+        }.addOnFailureListener {
+            binding.topImage.setImageResource(R.drawable.basketball_court)
+        }
+
+        binding.courseNameView.setText(item.nom)
+        binding.coursePriceView.setText("${item.preu} €")
 
         return binding.root
     }
